b1438616ea6a20e9a478911995a7642da8ad1c35
Usman Adrees <107811214+Usman3975@users.noreply.github.com>
2023-02-18 16:28:16 +0500

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { ChakraProvider } from '@chakra-ui/react'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        <ChakraProvider>
+        <Header/>
+        {children}
+        <Footer/>
+        </ChakraProvider>
+        </body>
+    </html>
+  )
+}
